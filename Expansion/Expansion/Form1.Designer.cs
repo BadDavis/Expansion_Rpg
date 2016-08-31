befdde421023fffe24326218b1cea85c47d697f7
BadDavis <davis693@o2.pl>
2016-08-31 18:17:33 +0200

    pierwsze przedmioty

---
+++ Expansion/Expansion/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Expansion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerBox = new System.Windows.Forms.PictureBox();
+            this.axeBox = new System.Windows.Forms.PictureBox();
+            this.swordBox = new System.Windows.Forms.PictureBox();
+            this.ghostBox = new System.Windows.Forms.PictureBox();
+            this.batBox = new System.Windows.Forms.PictureBox();
+            this.redPotionBox = new System.Windows.Forms.PictureBox();
+            this.bluePotionBox = new System.Windows.Forms.PictureBox();
+            this.ghoulBox = new System.Windows.Forms.PictureBox();
+            this.bowBox = new System.Windows.Forms.PictureBox();
+            this.bombBox = new System.Windows.Forms.PictureBox();
+            this.maceBox = new System.Windows.Forms.PictureBox();
+            this.shieldBox = new System.Windows.Forms.PictureBox();
+            this.wizardBox = new System.Windows.Forms.PictureBox();
+            this.quiverBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swordBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ghostBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.batBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPotionBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePotionBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ghoulBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bowBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bombBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maceBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shieldBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quiverBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerBox
+            // 
+            this.playerBox.BackColor = System.Drawing.Color.Transparent;
+            this.playerBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playerBox.BackgroundImage")));
+            this.playerBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.playerBox.Location = new System.Drawing.Point(129, 109);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(30, 30);
+            this.playerBox.TabIndex = 0;
+            this.playerBox.TabStop = false;
+            this.playerBox.Visible = false;
+            // 
+            // axeBox
+            // 
+            this.axeBox.BackColor = System.Drawing.Color.Transparent;
+            this.axeBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("axeBox.BackgroundImage")));
+            this.axeBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.axeBox.Location = new System.Drawing.Point(201, 109);
+            this.axeBox.Name = "axeBox";
+            this.axeBox.Size = new System.Drawing.Size(30, 30);
+            this.axeBox.TabIndex = 1;
+            this.axeBox.TabStop = false;
+            this.axeBox.Visible = false;
+            // 
+            // swordBox
+            // 
+            this.swordBox.BackColor = System.Drawing.Color.Transparent;
+            this.swordBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("swordBox.BackgroundImage")));
+            this.swordBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.swordBox.Location = new System.Drawing.Point(165, 109);
+            this.swordBox.Name = "swordBox";
+            this.swordBox.Size = new System.Drawing.Size(30, 30);
+            this.swordBox.TabIndex = 2;
+            this.swordBox.TabStop = false;
+            this.swordBox.Visible = false;
+            // 
+            // ghostBox
+            // 
+            this.ghostBox.BackColor = System.Drawing.Color.Transparent;
+            this.ghostBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ghostBox.BackgroundImage")));
+            this.ghostBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ghostBox.Location = new System.Drawing.Point(165, 169);
+            this.ghostBox.Name = "ghostBox";
+            this.ghostBox.Size = new System.Drawing.Size(30, 30);
+            this.ghostBox.TabIndex = 3;
+            this.ghostBox.TabStop = false;
+            this.ghostBox.Visible = false;
+            // 
+            // batBox
+            // 
+            this.batBox.BackColor = System.Drawing.Color.Transparent;
+            this.batBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("batBox.BackgroundImage")));
+            this.batBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.batBox.Location = new System.Drawing.Point(129, 169);
+            this.batBox.Name = "batBox";
+            this.batBox.Size = new System.Drawing.Size(30, 30);
+            this.batBox.TabIndex = 4;
+            this.batBox.TabStop = false;
+            this.batBox.Visible = false;
+            // 
+            // redPotionBox
+            // 
+            this.redPotionBox.BackColor = System.Drawing.Color.Transparent;
+            this.redPotionBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redPotionBox.BackgroundImage")));
+            this.redPotionBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redPotionBox.Location = new System.Drawing.Point(129, 216);
+            this.redPotionBox.Name = "redPotionBox";
+            this.redPotionBox.Size = new System.Drawing.Size(30, 30);
+            this.redPotionBox.TabIndex = 5;
+            this.redPotionBox.TabStop = false;
+            this.redPotionBox.Visible = false;
+            // 
+            // bluePotionBox
+            // 
+            this.bluePotionBox.BackColor = System.Drawing.Color.Transparent;
+            this.bluePotionBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bluePotionBox.BackgroundImage")));
+            this.bluePotionBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bluePotionBox.Location = new System.Drawing.Point(165, 216);
+            this.bluePotionBox.Name = "bluePotionBox";
+            this.bluePotionBox.Size = new System.Drawing.Size(30, 30);
+            this.bluePotionBox.TabIndex = 6;
+            this.bluePotionBox.TabStop = false;
+            this.bluePotionBox.Visible = false;
+            // 
+            // ghoulBox
+            // 
+            this.ghoulBox.BackColor = System.Drawing.Color.Transparent;
+            this.ghoulBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ghoulBox.BackgroundImage")));
+            this.ghoulBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ghoulBox.Location = new System.Drawing.Point(201, 169);
+            this.ghoulBox.Name = "ghoulBox";
+            this.ghoulBox.Size = new System.Drawing.Size(30, 30);
+            this.ghoulBox.TabIndex = 7;
+            this.ghoulBox.TabStop = false;
+            this.ghoulBox.Visible = false;
+            // 
+            // bowBox
+            // 
+            this.bowBox.BackColor = System.Drawing.Color.Transparent;
+            this.bowBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bowBox.BackgroundImage")));
+            this.bowBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bowBox.Location = new System.Drawing.Point(251, 109);
+            this.bowBox.Name = "bowBox";
+            this.bowBox.Size = new System.Drawing.Size(30, 30);
+            this.bowBox.TabIndex = 8;
+            this.bowBox.TabStop = false;
+            this.bowBox.Visible = false;
+            // 
+            // bombBox
+            // 
+            this.bombBox.BackColor = System.Drawing.Color.Transparent;
+            this.bombBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bombBox.BackgroundImage")));
+            this.bombBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bombBox.Location = new System.Drawing.Point(298, 109);
+            this.bombBox.Name = "bombBox";
+            this.bombBox.Size = new System.Drawing.Size(30, 30);
+            this.bombBox.TabIndex = 9;
+            this.bombBox.TabStop = false;
+            this.bombBox.Visible = false;
+            // 
+            // maceBox
+            // 
+            this.maceBox.BackColor = System.Drawing.Color.Transparent;
+            this.maceBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("maceBox.BackgroundImage")));
+            this.maceBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.maceBox.Location = new System.Drawing.Point(349, 109);
+            this.maceBox.Name = "maceBox";
+            this.maceBox.Size = new System.Drawing.Size(30, 30);
+            this.maceBox.TabIndex = 10;
+            this.maceBox.TabStop = false;
+            this.maceBox.Visible = false;
+            // 
+            // shieldBox
+            // 
+            this.shieldBox.BackColor = System.Drawing.Color.Transparent;
+            this.shieldBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("shieldBox.BackgroundImage")));
+            this.shieldBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.shieldBox.Location = new System.Drawing.Point(441, 109);
+            this.shieldBox.Name = "shieldBox";
+            this.shieldBox.Size = new System.Drawing.Size(30, 30);
+            this.shieldBox.TabIndex = 11;
+            this.shieldBox.TabStop = false;
+            this.shieldBox.Visible = false;
+            // 
+            // wizardBox
+            // 
+            this.wizardBox.BackColor = System.Drawing.Color.Transparent;
+            this.wizardBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wizardBox.BackgroundImage")));
+            this.wizardBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.wizardBox.Location = new System.Drawing.Point(251, 169);
+            this.wizardBox.Name = "wizardBox";
+            this.wizardBox.Size = new System.Drawing.Size(30, 30);
+            this.wizardBox.TabIndex = 12;
+            this.wizardBox.TabStop = false;
+            this.wizardBox.Visible = false;
+            // 
+            // quiverBox
+            // 
+            this.quiverBox.BackColor = System.Drawing.Color.Transparent;
+            this.quiverBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("quiverBox.BackgroundImage")));
+            this.quiverBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.quiverBox.Location = new System.Drawing.Point(385, 109);
+            this.quiverBox.Name = "quiverBox";
+            this.quiverBox.Size = new System.Drawing.Size(30, 30);
+            this.quiverBox.TabIndex = 15;
+            this.quiverBox.TabStop = false;
+            this.quiverBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 662);
+            this.Controls.Add(this.wizardBox);
+            this.Controls.Add(this.playerBox);
+            this.Controls.Add(this.ghoulBox);
+            this.Controls.Add(this.bluePotionBox);
+            this.Controls.Add(this.redPotionBox);
+            this.Controls.Add(this.batBox);
+            this.Controls.Add(this.ghostBox);
+            this.Controls.Add(this.swordBox);
+            this.Controls.Add(this.axeBox);
+            this.Controls.Add(this.bowBox);
+            this.Controls.Add(this.bombBox);
+            this.Controls.Add(this.maceBox);
+            this.Controls.Add(this.quiverBox);
+            this.Controls.Add(this.shieldBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swordBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ghostBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.batBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPotionBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePotionBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ghoulBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bowBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bombBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maceBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shieldBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quiverBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox playerBox;
+        private System.Windows.Forms.PictureBox axeBox;
+        private System.Windows.Forms.PictureBox swordBox;
+        private System.Windows.Forms.PictureBox ghostBox;
+        private System.Windows.Forms.PictureBox batBox;
+        private System.Windows.Forms.PictureBox redPotionBox;
+        private System.Windows.Forms.PictureBox bluePotionBox;
+        private System.Windows.Forms.PictureBox ghoulBox;
+        private System.Windows.Forms.PictureBox bowBox;
+        private System.Windows.Forms.PictureBox bombBox;
+        private System.Windows.Forms.PictureBox maceBox;
+        private System.Windows.Forms.PictureBox shieldBox;
+        private System.Windows.Forms.PictureBox wizardBox;
+        private System.Windows.Forms.PictureBox quiverBox;
+    }
+}
+
