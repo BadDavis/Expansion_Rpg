5d731b39ece0de92ef73f741d35dbb8e34835513
BadDavis <davis693@o2.pl>
2016-09-01 12:24:31 +0200

    logika gry + poruszanie sie

--- Expansion/Expansion/Form1.Designer.cs
+++ Expansion/Expansion/Form1.Designer.cs
@@ -43,6 +43,23 @@
             this.shieldBox = new System.Windows.Forms.PictureBox();
             this.wizardBox = new System.Windows.Forms.PictureBox();
             this.quiverBox = new System.Windows.Forms.PictureBox();
+            this.eqBox4 = new System.Windows.Forms.PictureBox();
+            this.eqBox2 = new System.Windows.Forms.PictureBox();
+            this.eqBox5 = new System.Windows.Forms.PictureBox();
+            this.eqBox3 = new System.Windows.Forms.PictureBox();
+            this.eqBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.playerHitPoints = new System.Windows.Forms.Label();
+            this.batLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ghostLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ghostHitPoints = new System.Windows.Forms.Label();
+            this.ghoulHitPoints = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.wizardHp = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axeBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.swordBox)).BeginInit();
@@ -57,6 +74,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.shieldBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wizardBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.quiverBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // playerBox
@@ -227,6 +250,190 @@
             this.quiverBox.TabStop = false;
             this.quiverBox.Visible = false;
             // 
+            // eqBox4
+            // 
+            this.eqBox4.BackColor = System.Drawing.Color.Transparent;
+            this.eqBox4.Location = new System.Drawing.Point(293, 537);
+            this.eqBox4.Name = "eqBox4";
+            this.eqBox4.Size = new System.Drawing.Size(50, 50);
+            this.eqBox4.TabIndex = 16;
+            this.eqBox4.TabStop = false;
+            // 
+            // eqBox2
+            // 
+            this.eqBox2.BackColor = System.Drawing.Color.Transparent;
+            this.eqBox2.Location = new System.Drawing.Point(181, 537);
+            this.eqBox2.Name = "eqBox2";
+            this.eqBox2.Size = new System.Drawing.Size(50, 50);
+            this.eqBox2.TabIndex = 17;
+            this.eqBox2.TabStop = false;
+            // 
+            // eqBox5
+            // 
+            this.eqBox5.BackColor = System.Drawing.Color.Transparent;
+            this.eqBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.eqBox5.Location = new System.Drawing.Point(349, 537);
+            this.eqBox5.Name = "eqBox5";
+            this.eqBox5.Size = new System.Drawing.Size(50, 50);
+            this.eqBox5.TabIndex = 18;
+            this.eqBox5.TabStop = false;
+            // 
+            // eqBox3
+            // 
+            this.eqBox3.BackColor = System.Drawing.Color.Transparent;
+            this.eqBox3.Location = new System.Drawing.Point(237, 537);
+            this.eqBox3.Name = "eqBox3";
+            this.eqBox3.Size = new System.Drawing.Size(50, 50);
+            this.eqBox3.TabIndex = 19;
+            this.eqBox3.TabStop = false;
+            // 
+            // eqBox1
+            // 
+            this.eqBox1.BackColor = System.Drawing.Color.Transparent;
+            this.eqBox1.Location = new System.Drawing.Point(125, 537);
+            this.eqBox1.Name = "eqBox1";
+            this.eqBox1.Size = new System.Drawing.Size(50, 50);
+            this.eqBox1.TabIndex = 20;
+            this.eqBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.playerLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.playerHitPoints, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.batLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ghostLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ghostHitPoints, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ghoulHitPoints, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.wizardHp, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(677, 405);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 90);
+            this.tableLayoutPanel1.TabIndex = 21;
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playerLabel.Location = new System.Drawing.Point(3, 0);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(94, 17);
+            this.playerLabel.TabIndex = 0;
+            this.playerLabel.Text = "Gracz";
+            // 
+            // playerHitPoints
+            // 
+            this.playerHitPoints.AutoSize = true;
+            this.playerHitPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playerHitPoints.Location = new System.Drawing.Point(103, 0);
+            this.playerHitPoints.Name = "playerHitPoints";
+            this.playerHitPoints.Size = new System.Drawing.Size(94, 17);
+            this.playerHitPoints.TabIndex = 1;
+            this.playerHitPoints.Text = "label2";
+            // 
+            // batLabel
+            // 
+            this.batLabel.AutoSize = true;
+            this.batLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.batLabel.Location = new System.Drawing.Point(3, 17);
+            this.batLabel.Name = "batLabel";
+            this.batLabel.Size = new System.Drawing.Size(94, 17);
+            this.batLabel.TabIndex = 2;
+            this.batLabel.Text = "Nietoperz";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(103, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // ghostLabel
+            // 
+            this.ghostLabel.AutoSize = true;
+            this.ghostLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ghostLabel.Location = new System.Drawing.Point(3, 34);
+            this.ghostLabel.Name = "ghostLabel";
+            this.ghostLabel.Size = new System.Drawing.Size(94, 17);
+            this.ghostLabel.TabIndex = 4;
+            this.ghostLabel.Text = "Duch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Upiór";
+            // 
+            // ghostHitPoints
+            // 
+            this.ghostHitPoints.AutoSize = true;
+            this.ghostHitPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ghostHitPoints.Location = new System.Drawing.Point(103, 51);
+            this.ghostHitPoints.Name = "ghostHitPoints";
+            this.ghostHitPoints.Size = new System.Drawing.Size(94, 17);
+            this.ghostHitPoints.TabIndex = 5;
+            this.ghostHitPoints.Text = "label2";
+            // 
+            // ghoulHitPoints
+            // 
+            this.ghoulHitPoints.AutoSize = true;
+            this.ghoulHitPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ghoulHitPoints.Location = new System.Drawing.Point(103, 34);
+            this.ghoulHitPoints.Name = "ghoulHitPoints";
+            this.ghoulHitPoints.Size = new System.Drawing.Size(94, 17);
+            this.ghoulHitPoints.TabIndex = 7;
+            this.ghoulHitPoints.Text = "label3";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.groupBox1.Location = new System.Drawing.Point(424, 505);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Czarodziej";
+            // 
+            // wizardHp
+            // 
+            this.wizardHp.AutoSize = true;
+            this.wizardHp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardHp.Location = new System.Drawing.Point(103, 68);
+            this.wizardHp.Name = "wizardHp";
+            this.wizardHp.Size = new System.Drawing.Size(94, 22);
+            this.wizardHp.TabIndex = 9;
+            this.wizardHp.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -234,6 +441,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1000, 662);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.eqBox1);
+            this.Controls.Add(this.eqBox3);
+            this.Controls.Add(this.eqBox5);
+            this.Controls.Add(this.eqBox2);
+            this.Controls.Add(this.eqBox4);
             this.Controls.Add(this.wizardBox);
             this.Controls.Add(this.playerBox);
             this.Controls.Add(this.ghoulBox);
@@ -265,6 +479,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.shieldBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.wizardBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.quiverBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eqBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -285,6 +506,23 @@
         private System.Windows.Forms.PictureBox shieldBox;
         private System.Windows.Forms.PictureBox wizardBox;
         private System.Windows.Forms.PictureBox quiverBox;
+        private System.Windows.Forms.PictureBox eqBox4;
+        private System.Windows.Forms.PictureBox eqBox2;
+        private System.Windows.Forms.PictureBox eqBox5;
+        private System.Windows.Forms.PictureBox eqBox3;
+        private System.Windows.Forms.PictureBox eqBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.Label playerHitPoints;
+        private System.Windows.Forms.Label batLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ghostLabel;
+        private System.Windows.Forms.Label ghostHitPoints;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ghoulHitPoints;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label wizardHp;
     }
 }
 
