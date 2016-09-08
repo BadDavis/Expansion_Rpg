90e7e720616188257041b0922d6bdf1bdea28fd2
BadDavis <davis693@o2.pl>
2016-09-09 00:03:53 +0200

    updatecharacters

--- Expansion/Expansion/Form1.cs
+++ Expansion/Expansion/Form1.cs
@@ -76,15 +76,19 @@ namespace Expansion
         {
             if (game.CheckPlayerInventory("Niebieska mikstura"))
             {
+                game.Equip("Niebieska mikstura");
             }
+            eqBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+
         }
 
         private void eqBox6_Click(object sender, EventArgs e)
         {
             if (game.CheckPlayerInventory("Czerwona mikstura"))
             {
-                
+                game.Equip("Czerwona mikstura");
             }
+            eqBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
         }
 
         private void RemoveInventorySpriteBorders()
@@ -95,6 +99,64 @@ namespace Expansion
             eqBox4.BorderStyle = BorderStyle.None;
             eqBox5.BorderStyle = BorderStyle.None;
             eqBox6.BorderStyle = BorderStyle.None;
+        }
+
+        public void UpdateCharacters()
+        {
+            playerBox.Location = game.playerLocation;
+            playerHitPoints.Text = game.playerHitPoints.ToString();
+
+            bool showBat = false;
+            bool showGhost = false;
+            bool showGhoul = false;
+            int enemiesShown = 0;
+
+            if (batBox.Visible == true)
+            {
+                showBat = true;
+            }
+            if (ghostBox.Visible)
+            {
+                showGhost = true;
+            }
+            if (ghoulBox.Visible == true)
+            {
+                showGhoul = true;
+            }
+
+            foreach (Enemy enemy in game.Enemies)
+            {
+                if (enemy is Bat)
+                {
+                    batBox.Location = enemy.Location;
+                    label1.Text = enemy.HitPoints.ToString();
+                    if (enemy.HitPoints > 0)
+                    {
+                        showBat = true;
+                        enemiesShown++;
+                    }
+                }
+                if (enemy is Ghost)
+                {
+                    ghostBox.Location = enemy.Location;
+                    label2.Text = enemy.HitPoints.ToString();
+                    if (enemy.HitPoints > 0)
+                    {
+                        showGhost = true;
+                        enemiesShown++;
+                    }
+                }
+                if (enemy is Ghoul)
+                {
+                    ghoulBox.Location = enemy.Location;
+                    label3.Text = enemy.HitPoints.ToString();
+                    if (enemy.HitPoints > 0)
+                    {
+                        showGhoul = true;
+                        enemiesShown++;
+                    }
+                }
+            }
         }
     }
 }
